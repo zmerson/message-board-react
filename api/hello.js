45270994a3e3cb18219de129f2734f434ae45d57
zach <zemerson@uco.edu>
2024-08-28 01:10:27 -0500

    vercel

---
+++ api/hello.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  res.json({ message: 'Hello, world!' });
+} 
