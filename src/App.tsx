a338ea91d3a53b399828b26f94fc6a432f7fcc54
zach <zemerson@uco.edu>
2024-09-24 22:42:23 -0500

    prod api base url

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import './App.css';
 import './output.css';
 import TagList from './components/Tags/TagList';
 
-axios.defaults.baseURL = '';
+axios.defaults.baseURL = 'linux.zemersonapp.online/api';
 
 // Set default headers for all requests (e.g., for JWT authentication)
 //axios.defaults.headers.common['Authorization'] = 'Bearer ' + YOUR_JWT_TOKEN;
