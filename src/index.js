b361a29d62273ebbfc82d287ed2143e130b49306
zmerson <zemerson@uco.edu>
2024-09-27 17:15:11 -0500

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-  <React.StrictMode>
+  <React>
     {/* <AuthProvider>
       <AuthWrapper> */}
       { <App></App>}
       {/* </AuthWrapper>
     </AuthProvider> */}
-  </React.StrictMode>
+  </React>
 );
 
 // If you want to start measuring performance in your app, pass a function
